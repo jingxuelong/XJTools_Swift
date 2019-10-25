c9c370d051eec54eb1aba663df64172614bf8ce1
jingxuelong <jingxuelong0037@gmail.com>
2019-10-25 15:32:55 +0800

    1.修复状态栏异常

--- XJTools_Swift/Classes/XJSwiftConst.swift
+++ XJTools_Swift/Classes/XJSwiftConst.swift
@@ -9,29 +9,29 @@ import Foundation
 
 //常量
 
-var k_XJScreenWide_S:CGFloat{
-    return UIScreen.main.bounds.size.width;
+var k_XJScreenWide_S: CGFloat{
+    return UIScreen.main.bounds.size.width
 }
-var k_XJScreenHeight_S:CGFloat{
-    return UIScreen.main.bounds.size.height;
+var k_XJScreenHeight_S: CGFloat{
+    return UIScreen.main.bounds.size.height
 }
-var k_XJStatusBarHeight_S:CGFloat{
-    return UIApplication.shared.statusBarFrame.size.height
+var k_XJStatusBarHeight_S: CGFloat{
+    return is_XJIphoneWithSafeArea_S ? 44.0:20.0
 }
-var k_XJNavHeight_S:CGFloat{
+var k_XJNavHeight_S: CGFloat{
     return 44.0
 }
-var k_XJNavAndStatusHeight_S:CGFloat{
-    return k_XJStatusBarHeight_S+k_XJNavHeight_S;
+var k_XJNavAndStatusHeight_S: CGFloat{
+    return k_XJStatusBarHeight_S+k_XJNavHeight_S
 }
-var k_XJTabbarHeight_S:CGFloat{
-    return 49.0;
+var k_XJTabbarHeight_S: CGFloat{
+    return 49.0
 }
-var k_XJTabbarSafeAreaHeight_S:CGFloat{
-    return is_XJIphoneWithSafeArea_S ?34.0:0.0;
+var k_XJTabbarSafeAreaHeight_S: CGFloat{
+    return is_XJIphoneWithSafeArea_S ?34.0:0.0
 }
-var k_XJTabbarAndSafeAreaHeight_S:CGFloat{
-    return k_XJTabbarHeight_S+k_XJTabbarSafeAreaHeight_S;
+var k_XJTabbarAndSafeAreaHeight_S: CGFloat{
+    return k_XJTabbarHeight_S+k_XJTabbarSafeAreaHeight_S
 }
 var XJWideScale: CGFloat{
     return UIScreen.main.bounds.width/375.0
@@ -39,11 +39,11 @@ var XJWideScale: CGFloat{
 
 
 //bool判断
-var  is_XJIphone_S:Bool {
+var  is_XJIphone_S: Bool {
     return UIDevice.current.userInterfaceIdiom == .phone
 }
 
-var  is_XJIphoneWithSafeArea_S:Bool {
+var  is_XJIphoneWithSafeArea_S: Bool {
     if is_XJIphone_S , #available(iOS 11.0, *){
         if Double((UIApplication.shared.delegate?.window??.safeAreaInsets.bottom)!) > 0.0 {
             return true
